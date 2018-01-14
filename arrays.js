a095d28075aef27e0bc5760d54bf823bcf1d8514
Anne van Gorkom <anne@vangorkom.com>
2018-01-14 17:21:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@ var chocolateBars = [
 
 function addElementToBeginningOfArray("bar", 1) {
   var chocolateBars = bar.unshift("mars")
-  return array
+  return chocolateBars
 }
 
 //function destructivelyAddElementToBeginningOfArray(chocolateBars, bar) {
